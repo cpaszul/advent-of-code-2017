ff1c43b064e2c7b8550076190158fa47eec791d5
Cory Paszul <cpaszul@gmail.com>
2017-12-15 14:51:07 -0500

    Updated day 15 to improve speed

--- day15.py
+++ day15.py
@@ -2,17 +2,12 @@ DEFAULT_INPUT = 'day15.txt'
 
 def part_1(loc=DEFAULT_INPUT):
     with open(loc) as f:
-        gen_a = int(f.readline().split(' ')[-1])
-        gen_b = int(f.readline().split(' ')[-1])
-    matching = 0
-    for _ in range(40000000):
-        gen_a *= 16807
-        gen_a %= 2147483647
-        gen_b *= 48271
-        gen_b %= 2147483647
-        if bin(gen_a)[-16:] == bin(gen_b)[-16:]:
-            matching += 1
-    return matching
+        a = int(f.readline().split(' ')[-1])
+        b = int(f.readline().split(' ')[-1])
+    gen_a = gen(a, 16807)
+    gen_b = gen(b, 48271)
+    return sum((next(gen_a) & 0xffff) == (next(gen_b) & 0xffff)
+               for _ in range(40000000))
 
 def part_2(loc=DEFAULT_INPUT):
     with open(loc) as f:
@@ -20,15 +15,10 @@ def part_2(loc=DEFAULT_INPUT):
         b = int(f.readline().split(' ')[-1])
     gen_a = gen(a, 16807, 4)
     gen_b = gen(b, 48271, 8)
-    matching = 0
-    for _ in range(5000000):
-        val_a = next(gen_a)
-        val_b = next(gen_b)
-        if bin(val_a)[-16:] == bin(val_b)[-16:]:
-            matching += 1
-    return matching
+    return sum((next(gen_a) & 0xffff) == (next(gen_b) & 0xffff)
+               for _ in range(5000000))
 
-def gen(value, multiplier, multiple):
+def gen(value, multiplier, multiple=1):
     while True:
         value *= multiplier
         value %= 2147483647
@@ -36,5 +26,5 @@ def gen(value, multiplier, multiple):
             yield value
 
 if __name__ == '__main__':
-    #print('Solution for Part One:', part_1())
+    print('Solution for Part One:', part_1())
     print('Solution for Part Two:', part_2())
